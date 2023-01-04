78653a4d2784b30a3729102d0ee644e026e9025a
matriuxsa <117915689+matriuxsa@users.noreply.github.com>
2023-01-04 17:37:50 +0100

    nuevo
    nnn

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert("PAGINA EN MANTENIMIENTO")
+// Obtener elementos del formulario
+const form = document.querySelector('.register-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
+const successMessage = document.querySelector('.success-message');
+const errorMessage = document.querySelector('.error-message');
